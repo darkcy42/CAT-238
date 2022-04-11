52213a97da191365dabb60d7aea9385815050df7
darkcy42 <darkcy42@gmail.com>
2022-04-11 15:26:58 -0700

    Updated game
    gammme

--- CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
+++ CAT238-FPS/Assets/Project/Scripts/Game/GameManager.cs
@@ -7,14 +7,18 @@ public class GameManager : MonoBehaviour
 {
     [Header("Game")]
     public Player player;
+    public GameObject enemySpawner;
     [Header("UI")]
     public Text ammoText;
     public Text healthText;
+    public Text enemyText;
+    public Text infoText;
+  
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        infoText.gameObject.SetActive(false);
     }
 
     // Update is called once per frame
@@ -22,5 +26,32 @@ public class GameManager : MonoBehaviour
     {
         ammoText.text = "Ammo: " + player.Ammo;
         healthText.text = "Health: " + player.Health;
+       
+        int killedEnemies = 0;
+        foreach(Enemy enemy in enemySpawner.GetComponentsInChildren<Enemy>())
+        if(enemy.Killed == false)
+        {
+                killedEnemies++;
+            }
+        enemyText.text = "Enemies: " + killedEnemies;
+
+        if(killedEnemies == 0)
+        {
+            infoText.gameObject.SetActive(true);
+            //infoText.text = "You win!\n Good Job!";
+        }
+
+        if(player.Killed == true)
+        {
+            infoText.gameObject.SetActive(true);
+            infoText.text = "You lose!";
+        }
+
+
+
+
     }
+
+  
+
 }
